e3308dad2740eb8c8bd4b0da4864a98e4d96a099
Sean Dez <sd@seandezoysa.com>
2018-12-23 23:56:44 +0700

    minor commenting update

--- routes/index.js
+++ routes/index.js
@@ -33,67 +33,18 @@ router.post('/test', (req, res, next) => {
 // const corsOptions = {origin : true, optionsSuccessStatus : 200 }; // CORS policy set broadly in app.js
 
 router.post('/api/donate', (req, res, next) => {
-  // step 1: wake up the cold-start server
+  // step 1: wake up hibernating back-end on initial load of the client app
   // step 2: create a stripe charge. save donor and email objects
   // step 3: save referees as email documents
   
   if (req.body.coldStart) {
-    null
+    console.log("cold start request received");
+    res.json({ message : 'Server has been cold-started' })
   } else if (req.body.checkoutStep === 'paymentDetails') {
       controllers.chargeAndSaveDonorInfo(req, res, null);
   } else if (req.body.checkoutStep === 'referrals') {
       controllers.saveReferrals(req, res, null);
   }
-  
-  
-  // if (req.body.step === '1') {
-  //
-  //
-  // } else if (req.body.step === '2' || req.body.checkoutStep === 'paymentDetails') {
-  //
-  //
-  //
-  //
-  // } else if (req.body.step === '3' || req.body.checkoutStep === 'referrals') {
-  //     console.log("received a post request to referral step. req.body: ", req.body);
-  //
-  //     // create an array with truthy email objects
-  //     let rawNameAndEmailArray = [
-  //       { firstName : req.body.referral1FirstName, email : req.body.referral1EmailAddress },
-  //       { firstName : req.body.referral2FirstName, email : req.body.referral2EmailAddress },
-  //     ];
-  //     let truthyEmailFilteredArray = [];
-  //     for (let i = 0; i < rawNameAndEmailArray.length; i ++) {
-  //       if (!!rawNameAndEmailArray[i].email) {
-  //         truthyEmailFilteredArray.push(rawNameAndEmailArray[i])
-  //       }
-  //     } // now only the pairs with an email are in the new array
-  //
-  //
-  //     // new loop to create a model instance and push into the db
-  //     for (let i = 0; i < truthyEmailFilteredArray.length; i ++) {
-  //
-  //       // create an instance of the email model
-  //       const referredContact = new Email({
-  //         donorId : req.body.donorId,
-  //         isReferred : true,
-  //         firstName : truthyEmailFilteredArray[i].firstName,
-  //         email : truthyEmailFilteredArray[i].email
-  //       });
-  //       referredContact.save((error, emailDocument) => {
-  //         if (error) {
-  //           console.log('error', error);
-  //           return error.message || new Error('Express, index.js:146')
-  //         } else {
-  //           console.log(`document for index ${i}`, emailDocument);
-  //         }
-  //       })
-  //     }
-  //
-  //     res.json({
-  //       checkoutStep : 'articlesList'
-  //     })
-  // }
 });
 
 
